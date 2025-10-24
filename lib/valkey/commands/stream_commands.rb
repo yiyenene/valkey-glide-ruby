68047ff02a15a8c8c13b201144cde855dac7c189
yiyenene <goodbyeboredworld@aol.jp>
2025-10-24 14:22:03 +0900

    add xtrim, xdel, xrange, xrevrange

--- lib/valkey/commands/stream_commands.rb
+++ lib/valkey/commands/stream_commands.rb
@@ -76,6 +76,110 @@ class Valkey
         send_command(RequestType::X_ADD, args)
       end
 
+      # Trims older entries of the stream if needed.
+      #
+      # @example Without options
+      #   valkey.xtrim('mystream', 1000)
+      # @example With options
+      #   valkey.xtrim('mystream', 1000, approximate: true)
+      # @example With strategy
+      #   valkey.xtrim('mystream', '1-0', strategy: 'MINID')
+      #
+      # @overload xtrim(key, maxlen, strategy: 'MAXLEN', approximate: true)
+      #   @param key         [String]  the stream key
+      #   @param maxlen      [Integer] max length of entries
+      #   @param strategy    [String]  the limit strategy, must be MAXLEN
+      #   @param approximate [Boolean] whether to add `~` modifier of maxlen or not
+      #   @param limit       [Integer] maximum count of entries to be evicted
+      # @overload xtrim(key, minid, strategy: 'MINID', approximate: true)
+      #   @param key         [String]  the stream key
+      #   @param minid       [String]  minimum id of entries
+      #   @param strategy    [String]  the limit strategy, must be MINID
+      #   @param approximate [Boolean] whether to add `~` modifier of minid or not
+      #   @param limit       [Integer] maximum count of entries to be evicted
+      #
+      # @return [Integer] the number of entries actually deleted
+      def xtrim(key, len_or_id, strategy: 'MAXLEN', approximate: false, limit: nil)
+        strategy = strategy.to_s.upcase
+
+        args = [key, strategy]
+        args << '~' if approximate
+        args << len_or_id
+        args.concat(['LIMIT', limit]) if limit
+        send_command(RequestType::X_TRIM, args)
+      end
+
+      # Delete entries by entry ids.
+      #
+      # @example With splatted entry ids
+      #   valkey.xdel('mystream', '0-1', '0-2')
+      # @example With arrayed entry ids
+      #   valkey.xdel('mystream', ['0-1', '0-2'])
+      #
+      # @param key [String]        the stream key
+      # @param ids [Array<String>] one or multiple entry ids
+      #
+      # @return [Integer] the number of entries actually deleted
+      def xdel(key, *ids)
+        # To keep compatibility with Redis
+        raise TypeError, "key cannot be nil" if key.nil?
+
+        args = [key].concat(ids.flatten)
+        send_command(RequestType::X_DEL, args)
+      end
+
+      # Fetches entries of the stream in ascending order.
+      #
+      # @example Without options
+      #   valkey.xrange('mystream')
+      # @example With a specific start
+      #   valkey.xrange('mystream', '0-1')
+      # @example With a specific start and end
+      #   valkey.xrange('mystream', '0-1', '0-3')
+      # @example With count options
+      #   valkey.xrange('mystream', count: 10)
+      #
+      # @param key [String]  the stream key
+      # @param start [String]  first entry id of range, default value is `-`
+      # @param end [String]  last entry id of range, default value is `+`
+      # @param count [Integer] the number of entries as limit
+      #
+      # @return [Array<Array<String, Hash>>] the ids and entries pairs
+      def xrange(key, start = '-', range_end = '+', count: nil)
+        # To keep compatibility with Redis
+        raise TypeError, "key cannot be nil" if key.nil?
+
+        args = [key, start, range_end]
+        args.concat(['COUNT', count]) if count
+        send_command(RequestType::X_RANGE, args, &Utils::HashifyStreamEntries)
+      end
+
+      # Fetches entries of the stream in descending order.
+      #
+      # @example Without options
+      #   valkey.xrevrange('mystream')
+      # @example With a specific end
+      #   valkey.xrevrange('mystream', '0-3')
+      # @example With a specific end and start
+      #   valkey.xrevrange('mystream', '0-3', '0-1')
+      # @example With count options
+      #   valkey.xrevrange('mystream', count: 10)
+      #
+      # @param key [String]  the stream key
+      # @param end [String]  first entry id of range, default value is `+`
+      # @param start [String]  last entry id of range, default value is `-`
+      # @params count [Integer] the number of entries as limit
+      #
+      # @return [Array<Array<String, Hash>>] the ids and entries pairs
+      def xrevrange(key, range_end = '+', start = '-', count: nil)
+        # To keep compatibility with Redis
+        raise TypeError, "key cannot be nil" if key.nil?
+
+        args = [key, range_end, start]
+        args.concat(['COUNT', count]) if count
+        send_command(RequestType::X_REV_RANGE, args, &Utils::HashifyStreamEntries)
+      end
+
       # Manages the consumer group of the stream.
       #
       # @example With `create` subcommand
--- test/lint/stream_commands.rb
+++ test/lint/stream_commands.rb
@@ -148,5 +148,251 @@ module Lint
       assert_raises(Valkey::CommandError) { r.xgroup(nil, nil, nil) }
       assert_raises(Valkey::CommandError) { r.xgroup('', '', '') }
     end
+
+    def test_xtrim
+      r.xadd('s1', { f: 'v1' })
+      r.xadd('s1', { f: 'v2' })
+      r.xadd('s1', { f: 'v3' })
+      r.xadd('s1', { f: 'v4' })
+      assert_equal 2, r.xtrim('s1', 2)
+    end
+
+    def test_xtrim_with_approximate_option
+      r.xadd('s1', { f: 'v1' })
+      r.xadd('s1', { f: 'v2' })
+      r.xadd('s1', { f: 'v3' })
+      r.xadd('s1', { f: 'v4' })
+      assert_equal 0, r.xtrim('s1', 2, approximate: true)
+    end
+
+    def test_xtrim_with_limit_option
+      begin
+        original = r.config(:get, 'stream-node-max-entries')['stream-node-max-entries']
+        r.config(:set, 'stream-node-max-entries', 1)
+
+        r.xadd('s1', { f: 'v1' })
+        r.xadd('s1', { f: 'v2' })
+        r.xadd('s1', { f: 'v3' })
+        r.xadd('s1', { f: 'v4' })
+
+        assert_equal 1, r.xtrim('s1', 0, approximate: true, limit: 1)
+        error = assert_raises(Valkey::CommandError) { r.xtrim('s1', 0, limit: 1) }
+        assert_includes error.message, "syntax error, LIMIT cannot be used without the special ~ option"
+      ensure
+        r.config(:set, 'stream-node-max-entries', original)
+      end
+    end
+
+    def test_xtrim_with_maxlen_strategy
+      r.xadd('s1', { f: 'v1' }, id: '0-1')
+      r.xadd('s1', { f: 'v1' }, id: '0-2')
+      r.xadd('s1', { f: 'v1' }, id: '1-0')
+      r.xadd('s1', { f: 'v1' }, id: '1-1')
+      assert_equal(2, r.xtrim('s1', 2, strategy: 'MAXLEN'))
+    end
+
+    def test_xtrim_with_minid_strategy
+      r.xadd('s1', { f: 'v1' }, id: '0-1')
+      r.xadd('s1', { f: 'v1' }, id: '0-2')
+      r.xadd('s1', { f: 'v1' }, id: '1-0')
+      r.xadd('s1', { f: 'v1' }, id: '1-1')
+      assert_equal(2, r.xtrim('s1', '1-0', strategy: 'MINID'))
+    end
+
+    def test_xtrim_with_approximate_minid_strategy
+      r.xadd('s1', { f: 'v1' }, id: '0-1')
+      r.xadd('s1', { f: 'v1' }, id: '0-2')
+      r.xadd('s1', { f: 'v1' }, id: '1-0')
+      r.xadd('s1', { f: 'v1' }, id: '1-1')
+      assert_equal(0, r.xtrim('s1', '1-0', strategy: 'MINID', approximate: true))
+    end
+
+    def test_xtrim_with_invalid_strategy
+      r.xadd('s1', { f: 'v1' })
+      error = assert_raises(Valkey::CommandError) { r.xtrim('s1', '1-0', strategy: '') }
+      assert_includes error.message, "syntax error"
+    end
+
+    def test_xtrim_with_not_existed_stream
+      assert_equal 0, r.xtrim('not-existed-stream', 2)
+    end
+
+    def test_xtrim_with_invalid_arguments
+      assert_raises(Valkey::CommandError) { r.xtrim('', '') }
+      assert_equal 0, r.xtrim('s1', 0)
+      assert_raises(Valkey::CommandError) { r.xtrim('s1', -1, approximate: true) }
+    end
+
+    def test_xdel_with_splatted_entry_ids
+      r.xadd('s1', { f: '1' }, id: '0-1')
+      r.xadd('s1', { f: '2' }, id: '0-2')
+      assert_equal 2, r.xdel('s1', '0-1', '0-2', '0-3')
+    end
+
+    def test_xdel_with_arrayed_entry_ids
+      r.xadd('s1', { f: '1' }, id: '0-1')
+      assert_equal 1, r.xdel('s1', ['0-1', '0-2'])
+    end
+
+    def test_xdel_with_invalid_entry_ids
+      assert_equal 0, r.xdel('s1', 'invalid_format')
+    end
+
+    def test_xdel_with_invalid_arguments
+      assert_raises(TypeError) { r.xdel(nil, nil) }
+      assert_raises(TypeError) { r.xdel(nil, [nil]) }
+      assert_equal 0, r.xdel('', '')
+      assert_equal 0, r.xdel('', [''])
+      assert_raises(Valkey::CommandError) { r.xdel('s1', []) }
+    end
+
+    def test_xrange
+      r.xadd('s1', { f: 'v1' }, id: '0-1')
+      r.xadd('s1', { f: 'v2' }, id: '0-2')
+      r.xadd('s1', { f: 'v3' }, id: '0-3')
+
+      actual = r.xrange('s1')
+
+      assert_equal(%w(v1 v2 v3), actual.map { |i| i.last['f'] })
+    end
+
+    def test_xrange_with_start_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrange('s1', '0-2')
+
+      assert_equal %w(0-2 0-3), actual.map(&:first)
+    end
+
+    def test_xrange_with_end_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrange('s1', '-', '0-2')
+      assert_equal %w(0-1 0-2), actual.map(&:first)
+    end
+
+    def test_xrange_with_start_and_end_options
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrange('s1', '0-2', '0-2')
+
+      assert_equal %w(0-2), actual.map(&:first)
+    end
+
+    def test_xrange_with_incomplete_entry_id_options
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '1-1')
+      r.xadd('s1', { f: 'v' }, id: '2-1')
+
+      actual = r.xrange('s1', '0', '1')
+
+      assert_equal 2, actual.size
+      assert_equal %w(0-1 1-1), actual.map(&:first)
+    end
+
+    def test_xrange_with_count_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrange('s1', count: 2)
+
+      assert_equal %w(0-1 0-2), actual.map(&:first)
+    end
+
+    def test_xrange_with_not_existed_stream_key
+      assert_equal([], r.xrange('not-existed'))
+    end
+
+    def test_xrange_with_invalid_entry_id_options
+      assert_raises(Valkey::CommandError) { r.xrange('s1', 'invalid', 'invalid') }
+    end
+
+    def test_xrange_with_invalid_arguments
+      assert_raises(TypeError) { r.xrange(nil) }
+      assert_equal([], r.xrange(''))
+    end
+
+    def test_xrevrange
+      r.xadd('s1', { f: 'v1' }, id: '0-1')
+      r.xadd('s1', { f: 'v2' }, id: '0-2')
+      r.xadd('s1', { f: 'v3' }, id: '0-3')
+
+      actual = r.xrevrange('s1')
+
+      assert_equal %w(0-3 0-2 0-1), actual.map(&:first)
+      assert_equal(%w(v3 v2 v1), actual.map { |i| i.last['f'] })
+    end
+
+    def test_xrevrange_with_start_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrevrange('s1', '+', '0-2')
+
+      assert_equal %w(0-3 0-2), actual.map(&:first)
+    end
+
+    def test_xrevrange_with_end_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrevrange('s1', '0-2')
+
+      assert_equal %w(0-2 0-1), actual.map(&:first)
+    end
+
+    def test_xrevrange_with_start_and_end_options
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrevrange('s1', '0-2', '0-2')
+
+      assert_equal %w(0-2), actual.map(&:first)
+    end
+
+    def test_xrevrange_with_incomplete_entry_id_options
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '1-1')
+      r.xadd('s1', { f: 'v' }, id: '2-1')
+
+      actual = r.xrevrange('s1', '1', '0')
+
+      assert_equal 2, actual.size
+      assert_equal '1-1', actual.first.first
+    end
+
+    def test_xrevrange_with_count_option
+      r.xadd('s1', { f: 'v' }, id: '0-1')
+      r.xadd('s1', { f: 'v' }, id: '0-2')
+      r.xadd('s1', { f: 'v' }, id: '0-3')
+
+      actual = r.xrevrange('s1', count: 2)
+
+      assert_equal 2, actual.size
+      assert_equal '0-3', actual.first.first
+    end
+
+    def test_xrevrange_with_not_existed_stream_key
+      assert_equal([], r.xrevrange('not-existed'))
+    end
+
+    def test_xrevrange_with_invalid_entry_id_options
+      assert_raises(Valkey::CommandError) { r.xrevrange('s1', 'invalid', 'invalid') }
+    end
+
+    def test_xrevrange_with_invalid_arguments
+      assert_raises(TypeError) { r.xrevrange(nil) }
+      assert_equal([], r.xrevrange(''))
+    end
   end
 end

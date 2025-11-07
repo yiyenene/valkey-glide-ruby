bc40a746e5bd797bd8c08d363395c3ada3a01956
yiyenene <goodbyeboredworld@aol.jp>
2025-11-07 12:08:50 +0900

    fix fxxkin lint

--- test/lint/cluster_commands.rb
+++ test/lint/cluster_commands.rb
@@ -109,7 +109,7 @@ module Lint
       # Verify slots is an array
       assert_instance_of Array, first_shard["slots"]
       assert first_shard["slots"].length == 2,
-        "Slots should have start and end. Got: #{first_shard["slots"].inspect} (length: #{first_shard["slots"].length})"
+             "Slots should have start and end. Got: #{first_shard['slots'].inspect} (length: #{first_shard['slots'].length})"
       # Verify nodes is an array
       assert_instance_of Array, first_shard["nodes"]
       assert first_shard["nodes"].length >= 1, "Should have at least one node"
